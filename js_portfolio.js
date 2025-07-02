6a210b3d23081b183efa64a2e9fcd5a60a9efaf4
AbenthyL <abenthylilliestrom@gmail.com>
2025-07-02 15:28:33 +0200

    Split existing pages into separate html files with one "master" menu file that is loaded into each page via jQuery

--- js_portfolio.js
+++ js_portfolio.js
@@ -1,30 +1,15 @@
+// Load menu when document is ready
+  $(document).ready(function () {
+    $('#menu').load('menu.html');
+  });
+  
 function openPage(pageName, elmnt) {
-  // Hide all elements with class="content-page" by default */
-  var i, pagecontent, pagelinks;
-  pagecontent = document.getElementsByClassName("col-main");
-  for (i = 0; i < pagecontent.length; i++) {
-    pagecontent[i].style.display = "none";
-  }
-
   // Clear background color of previously clicked page button
   pagelinks = document.getElementsByClassName("page-link");
   for (i = 0; i < pagelinks.length; i++) {
     pagelinks[i].style.backgroundColor = "";
   }
 
-  // Show the specific page content
-  document.getElementById(pageName).style.display = "block";
-
   // Add the specific color to the button used to open the page content
   elmnt.style.backgroundColor = getComputedStyle(elmnt).getPropertyValue("--light");
 }
-
-function openMenu(menuName) {
-	if (document.getElementById(menuName).style.display == "block") {
-		document.getElementById(menuName).style.display = "none";
-	}
-	else {
-		document.getElementById(menuName).style.display = "block";
-	}
-}
-
