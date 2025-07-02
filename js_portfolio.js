1314a49bf2e13fe8174c5248728118d7f92fa3d8
AbenthyL <abenthylilliestrom@gmail.com>
2025-07-02 16:55:26 +0200

    better "menu missing" text and more functional mobile menu. update title and slightly update about

--- js_portfolio.js
+++ js_portfolio.js
@@ -3,13 +3,22 @@
     $('#menu').load('menu.html');
   });
   
-function openPage(pageName, elmnt) {
-  // Clear background color of previously clicked page button
+function setOpenPage(elmnt) {
+  // Clear background color of all page buttons
   pagelinks = document.getElementsByClassName("page-link");
   for (i = 0; i < pagelinks.length; i++) {
     pagelinks[i].style.backgroundColor = "";
   }
 
-  // Add the specific color to the button used to open the page content
+  // Set 'selected' color of chosen page button
   elmnt.style.backgroundColor = getComputedStyle(elmnt).getPropertyValue("--light");
+}
+
+function toggleMenu(menuName) {
+	if (document.getElementById(menuName).style.display == "block") {
+		document.getElementById(menuName).style.display = "none";
+	}
+	else {
+		document.getElementById(menuName).style.display = "block";
+	}
 }
