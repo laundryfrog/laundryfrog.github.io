f572ac3c4b6718bffd5d84c33ce20f38f591a855
AbenthyL <abenthylilliestrom@gmail.com>
2025-07-02 17:07:33 +0200

    remove unused js function

--- js_portfolio.js
+++ js_portfolio.js
@@ -2,18 +2,7 @@
   $(document).ready(function () {
     $('#menu').load('menu.html');
   });
-  
-function setOpenPage(elmnt) {
-  // Clear background color of all page buttons
-  pagelinks = document.getElementsByClassName("page-link");
-  for (i = 0; i < pagelinks.length; i++) {
-    pagelinks[i].style.backgroundColor = "";
-  }
-
-  // Set 'selected' color of chosen page button
-  elmnt.style.backgroundColor = getComputedStyle(elmnt).getPropertyValue("--light");
-}
-
+ 
 function toggleMenu(menuName) {
 	if (document.getElementById(menuName).style.display == "block") {
 		document.getElementById(menuName).style.display = "none";
